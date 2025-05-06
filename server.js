1675429fa85ce1e5b40f61ebc8761b1aebdac656
Bernardo Marques <sardinhaber@gmail.com>
2025-05-06 09:25:04 -0300

    feat: add multer

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const clienteRoutes = require("./src/routes/clienteRoute");
 const reservaRoutes = require("./src/routes/reservaRoute");
+const path = require("path");
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api", clienteRoutes);
 app.use("/api", reservaRoutes);
--- src/routes/clienteRoute.js
+++ src/routes/clienteRoute.js
@@ -4,7 +4,7 @@ const clienteController = require("../controllers/clienteController");
 
 router.get("/clientes", clienteController.getAllClientes);
 router.get("/clientes/:id", clienteController.getClienteById);
-router.post("/clientes", clienteController.createCliente);
+router.post("/clientes", upload.single("foto"), clienteController.createCliente);
 router.put("/clientes/:id", clienteController.updateCliente);
 router.delete("/clientes/:id", clienteController.deleteCliente);
 
